55962d9a76498901ab18632c72c568a78848c159
ARIAS LOBATO <jrarias@segulagrp.es>
2025-04-08 17:12:19 +0200

    frontend

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const routes = require("./routes/Routes");
 const conexionBD = require("./database/ConexionBD");
 const sequelize = require("./config/database");
 require("dotenv").config();
+const { obtenerPoligonosYLines } = require('./turf/turf.js');
 
 
 
@@ -22,6 +23,17 @@ app.use(express.json());
 
 // Configuración de las rutas de la aplicación
 app.use("/", routes);
+
+// Endpoint para obtener los polígonos y líneas de los rails
+app.get('/api/poligonos', (req, res) => {
+  try {
+    const data = obtenerPoligonosYLines();
+    res.json(data);  // Devuelve la información de los polígonos y las líneas
+  } catch (error) {
+    console.error('Error al obtener los polígonos:', error);
+    res.status(500).json({ error: 'Error al obtener los polígonos' });
+  }
+});
 
 // Establecer la conexión con la base de datos y sincronizarla
 conexionBD(sequelize, app, port);
